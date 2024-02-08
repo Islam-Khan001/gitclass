1f3956c2353944e54ab02087e030cf6aa056fc61
faze flynn <faiz.14a@gmail.com>
2024-02-08 19:43:43 +0530

    tracked program1

---
+++ program1.java
@@ -0,0 +1,11 @@
+
+
+public class program1 {
+
+    public static void main (String[] args){
+        // program1 t = new program1();
+
+        System.out.println("this is my first commit");
+        
+    }
+}
