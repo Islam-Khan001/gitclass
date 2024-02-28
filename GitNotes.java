e87e99bde4e24bb0d226061afc0f9ab8008aa647
faze flynn <faiz.14a@gmail.com>
2024-02-28 14:28:53 +0530

    html file commited

---
+++ GitNotes.java
@@ -0,0 +1,5 @@
+// use Remove-Item -Recurse -Force .git to remove connection from the remote
+// repo
+// rm .git/index.lock  -- to force a process to stop in terminal
+// del .git\index.lock -- to force a process to stop in windows
+// 
